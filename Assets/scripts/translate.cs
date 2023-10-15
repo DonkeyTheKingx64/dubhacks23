30736ed01236bcd827939199d81d6c2d37686623
codingleo727 <leowei.tx2@gmail.com>
2023-10-14 20:13:12 -0700

    Updated Using Wattson

--- Assets/scripts/translate.cs
+++ Assets/scripts/translate.cs
@@ -12,60 +12,59 @@ public class translate : MonoBehaviour
 
     private string lastChecked = "";
 
-    private const string apiUrl = "https://api.openai.com/v1/chat/completions";
-    private const string apiKey = "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";
+    private const string apiUrl = "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/23381c5d-acb8-4cd3-a014-15afe2cc3cc0/v3/translate?version=2018-05-01";
+    private const string apiKey = "27tYdhgUAlIdHT8AqC6Zn2T8oI1q_Q5LXtqg6dBsoF5j";
 
-    private void Start()
-    {
-        if (textToTranslate.text != lastChecked)
-        {
+    private void Start() {
+        if (textToTranslate.text != lastChecked) {
             lastChecked = textToTranslate.text;
 
-            if (!string.IsNullOrEmpty(lastChecked))
-            {
-                string prompt = $"Translate the following English text to French: '{lastChecked}', only outputting the translated word and nothing else";
-                StartCoroutine(GetTranslationFromGPT3(prompt));
+            if (!string.IsNullOrEmpty(lastChecked)) {
+                WatsonRequest request = new WatsonRequest();
+                request.text = new List<string>() { lastChecked };
+                request.source = "en";
+                request.target = "fr";
+                string jsonBody = JsonUtility.ToJson(request);
+                StartCoroutine(GetTranslationFromWatson(jsonBody));
             }
         }
     }
 
-    private IEnumerator GetTranslationFromGPT3(string prompt)
-    {
-        string jsonBody = $"{{\"prompt\":\"{prompt}\",\"max_tokens\":150}}";
-
-        using (UnityWebRequest www = new UnityWebRequest(apiUrl, "POST"))
-        {
+    private IEnumerator GetTranslationFromWatson(string jsonBody) {
+        using (UnityWebRequest www = new UnityWebRequest(apiUrl, "POST")) {
             byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonBody);
             www.uploadHandler = new UploadHandlerRaw(bodyRaw);
             www.downloadHandler = new DownloadHandlerBuffer();
             www.SetRequestHeader("Content-Type", "application/json");
-            www.SetRequestHeader("Authorization", "Bearer " + apiKey);
+            www.SetRequestHeader("Authorization", "Basic " + System.Convert.ToBase64String(Encoding.ASCII.GetBytes("apikey:" + apiKey)));
 
             yield return www.SendWebRequest();
 
-            if (www.result != UnityWebRequest.Result.Success)
-            {
+            if (www.result != UnityWebRequest.Result.Success) {
                 Debug.Log(www.error);
-                translatedText.text = "Error: " + www.error;
-            }
-            else
-            {
+                translatedText.text = "Error: " + www.error + "Reason: " + www.downloadHandler.text + "Body: " + jsonBody;
+            } else {
                 string response = www.downloadHandler.text;
-                string chatResponse = JsonUtility.FromJson<ChatResponse>(response).choices[0].text.Trim();
-                translatedText.text = chatResponse;
+                string watsonResponse = JsonUtility.FromJson<WatsonResponse>(response).translations[0].translation;
+                translatedText.text = watsonResponse;
             }
         }
     }
 
     [System.Serializable]
-    public class ChatResponse
-    {
-        public Choice[] choices;
+    public class WatsonRequest {
+        public List<string> text;
+        public string source;
+        public string target;
     }
 
     [System.Serializable]
-    public class Choice
-    {
-        public string text;
+    public class WatsonResponse {
+        public List<Translation> translations;
+    }
+
+    [System.Serializable]
+    public class Translation {
+        public string translation;
     }
 }
